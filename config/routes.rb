f5dccd4382e47b2f0f8644795bb8678f434e04f8
sootedninjas <sootedninjas@gmail.com>
2015-10-04 08:14:50 +0000

    create new and edit forms plus partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   root 'pages#home'
  get '/home', to: 'pages#home'
+ 
+ # get '/recipes', to: 'recipes#index'
+ # get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
+ # post '/recipes', to: 'recipes#create', as: 'create_recipe'
+ # get '/recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
+ # patch '/recipes/:id', to: 'recipes#update', as: 'update_recipe'
+ # get '/recipes/:id', to: 'recipes#show', as: 'show_recipe'
+ # delete '/recipes/:id', to: 'recipes#destroy', as: 'delete_recipe'
+ 
+ resources :recipes
+ resources :chefs
 end
+ 
